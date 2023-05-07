e0dcbc5357b4d1b161a4f82228b1bd7ec02df6a8
jearnest94 <jearnest1994@gmail.com>
2023-05-07 13:30:05 +0200

    added 2 resols

---
+++ 1280x1024_240hz.bat
@@ -0,0 +1,23 @@
+@ECHO off
+TITLE Screen Resolution Changer v1
+
+REM ####################################
+REM    SET HERE THE RESOLUTION WANTED
+REM ####################################
+
+SET x=1280
+SET y=1024
+SET r=240
+
+REM ####################################
+REM      USING QRES TO CHANGE RES
+REM ####################################
+
+QRes.exe /x:%x% /y:%y% /r:%r%
+
+CLS
+
+ECHO.
+ECHO Changing Resolution to %x% x %y%... @ %r%Hz
+ECHO.
+timeout 3
